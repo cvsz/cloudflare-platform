fe2eaf0794d2647ceb1743d25fb696dbe5a2f677
cvsz <seaza@msn.com>
2026-05-15 19:20:45 +0700

    Add api-zveo hostname to DNS import helper

--- ops/scripts/import-existing-tunnel-dns.sh
+++ ops/scripts/import-existing-tunnel-dns.sh
@@ -10,6 +10,7 @@ records=(
   "www.zeaz.dev|cloudflare_record.tunnel_cname[\"www.zeaz.dev\"]"
   "app.zeaz.dev|cloudflare_record.tunnel_cname[\"app.zeaz.dev\"]"
   "admin-wallet.zeaz.dev|cloudflare_record.tunnel_cname[\"admin-wallet.zeaz.dev\"]"
+  "api-zveo.zeaz.dev|cloudflare_record.tunnel_cname[\"api-zveo.zeaz.dev\"]"
   "api.zveo.zeaz.dev|cloudflare_record.tunnel_cname[\"api.zveo.zeaz.dev\"]"
   "api.zeaz.dev|module.dns.cloudflare_record.records[\"api\"]"
 )
