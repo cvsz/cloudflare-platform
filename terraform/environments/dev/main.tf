319111f2b2440116b2dbebb564a4c24ec70be77b
cvsz <seaza@msn.com>
2026-05-28 15:20:37 +0700

    fix(terraform): remove unused dev environment variables

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -13,9 +13,6 @@ provider "cloudflare" {
   api_token = var.cloudflare_bootstrap_token
 }
 
-variable "cloudflare_account_id" { type = string }
-variable "cloudflare_zone_id" { type = string }
-
 variable "cloudflare_bootstrap_token" {
   type      = string
   sensitive = true
